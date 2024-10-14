6c17ba14ac00817de8acf00c619ec8e6f2beb328
ivyhanohan <msivyquino@gmail.com>
2024-10-14 16:19:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,42 @@
+
 <template>
   <div id="app">
-    <router-view></router-view>
+    <!-- Navigation Bar -->
+    <nav>
+      <!-- Use router-link to navigate between routes -->
+      <router-link to="/home">Home</router-link>
+      <router-link to="/products">Products</router-link>
+    </nav>
+
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+nav {
+  background-color: aquamarine;
+  padding: 10px;
+}
+
+nav a {
+  color: white;
+  margin-right: 15px;
+  text-decoration: none;
+}
+
+nav a:hover {
+  text-decoration: underline;
+  background-color: azure;
+  color: black;
+}
+nav:hover {
+  background-color: gray;
+}
+</style>
+
