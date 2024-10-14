e484adf2d78d7a3fe52d7f4fa0d68283331ca027
ivyhanohan <msivyquino@gmail.com>
2024-10-14 16:25:00 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router' // Import the router
 
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
+const app = createApp(App)
 
+// Use the router
+app.use(router)
 
+app.mount('#app')
 
 
